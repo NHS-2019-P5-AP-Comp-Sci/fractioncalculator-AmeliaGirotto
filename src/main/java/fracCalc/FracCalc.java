f5673c22407fa227ad3a65c34a8117b6dd5e0786
s-girottoa <s-girottoa@53S500037924.it.bsd405.org>
2020-01-22 17:16:49 -0800

    Checkpoint 2

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -10,11 +10,14 @@ public class FracCalc {
 
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
-		System.out.println("Write your math problem here: ");
-		String equation = userInput.nextLine();
+		String equation = "";
+		while (!equation.equals("quit")) {
+			System.out.println("Write your math problem here or type quit to be done: ");
+			equation = userInput.nextLine();
+			String answer = produceAnswer(equation);
+			System.out.println(answer);
+		}
 		userInput.close();
-		String answer = produceAnswer(equation);
-		System.out.println(answer);
 	}
 
 	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
@@ -29,7 +32,14 @@ public class FracCalc {
 	// calculated
 	// e.g. return ==> "1_1/4"
 	public static String produceAnswer(String e) {
-        String frac1 = "";
+        String solution = "";
+		String fraction2 = processFractions(e);
+        solution = splitFractions(fraction2);
+        return solution;
+    }
+	
+	public static String processFractions(String e) {
+		String frac1 = "";
         String frac2 = "";
         	
 		for (int i = 0; i < e.length(); i++) {
@@ -37,14 +47,34 @@ public class FracCalc {
         	if (a == '+' || a == '-' || a == '*') {
         		frac1 = e.substring(0, i - 1);
         		char operator = e.charAt(i);
-        		frac2 = e.substring(i + 1);
+        		frac2 = e.substring(i + 2);
         	}
         }
     	
         return frac2;
-    }
+	}
+	
+	public static String splitFractions(String f2) {
+		int whole = 0;
+		int numerator = 0;
+		int denominator = 0;
+		String output = "";
+		if (f2.contains("_")) {
+			whole = Integer.parseInt(f2.substring(0,f2.indexOf("_")));
+			output += "whole:" + whole;
+		}
+		if (f2.contains("/")) {
+			numerator = Integer.parseInt(f2.substring(f2.indexOf("_") + 1, f2.indexOf("/")));
+			output += " numerator:" + numerator;
+			denominator = Integer.parseInt(f2.substring(f2.indexOf("/") + 1));
+			output += " denominator:" + denominator;
+		}
+		return output;
+			
+	}
 
 	// TODO: Fill in the space below with any helper methods that you think you will
 	// need
 
 }
+ 
