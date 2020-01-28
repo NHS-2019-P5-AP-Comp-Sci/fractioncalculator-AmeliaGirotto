290dbd794f1060726de4207e58d8fc8cde480fb3
s-girottoa <s-girottoa@53S500037924.it.bsd405.org>
2020-01-27 21:04:41 -0800

    checkpoint 3

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -32,49 +32,179 @@ public class FracCalc {
 	// calculated
 	// e.g. return ==> "1_1/4"
 	public static String produceAnswer(String e) {
-        String solution = "";
-		String fraction2 = processFractions(e);
-        solution = splitFractions(fraction2);
-        return solution;
-    }
-	
-	public static String processFractions(String e) {
-		String frac1 = "";
-        String frac2 = "";
-        	
-		for (int i = 0; i < e.length(); i++) {
-        	char a = e.charAt(i);
-        	if (a == '+' || a == '-' || a == '*') {
-        		frac1 = e.substring(0, i - 1);
-        		char operator = e.charAt(i);
-        		frac2 = e.substring(i + 2);
-        	}
-        }
-    	
-        return frac2;
+		String solution = "";
+		String fraction1 = processFraction1(e);
+		String fraction2 = processFraction2(e);
+		String operator = processOperator(e);
+
+		solution = splitFractions(fraction1, fraction2, operator);
+		return solution;
 	}
-	
-	public static String splitFractions(String f2) {
-		int whole = 0;
-		int numerator = 0;
-		int denominator = 0;
-		String output = "";
-		if (f2.contains("_")) {
-			whole = Integer.parseInt(f2.substring(0,f2.indexOf("_")));
-			output += "whole:" + whole;
+
+	public static String processFraction1(String e) {
+		String frac1 = "";
+
+		for (int i = 0; i < e.length(); i++) {
+			char a = e.charAt(i);
+			if (a == '+' || a == '-' || a == '*') {
+				frac1 = e.substring(0, i - 1);
+			}
 		}
-		if (f2.contains("/")) {
-			numerator = Integer.parseInt(f2.substring(f2.indexOf("_") + 1, f2.indexOf("/")));
-			output += " numerator:" + numerator;
-			denominator = Integer.parseInt(f2.substring(f2.indexOf("/") + 1));
-			output += " denominator:" + denominator;
+		
+		return frac1;
+	}
+
+	public static String processFraction2(String e) {
+		String frac2 = "";
+
+		for (int i = 0; i < e.length(); i++) {
+			char a = e.charAt(i);
+			if (a == '+' || a == '-' || a == '*') {
+
+				frac2 = e.substring(i + 2);
+			}
 		}
-		return output;
+		
+		return frac2;
+	}
+
+	public static String processOperator(String e) {
+		String operator = "/";
+
+		for (int i = 0; i < e.length(); i++) {
+			char a = e.charAt(i);
+			if (a == '+' || a == '-' || a == '*') {
+
+				operator = e.substring(i, i + 1);
+
+			}
+
+		}
+
+		return operator;
+	}
+
+	public static String splitFractions(String f1, String f2, String op) {
+		int whole1 = 0;
+		int numerator1 = 0;
+		int denominator1 = 0;
+		String operator = op;
+		String answer = "";
+
+		if (f1.contains("_")) {
+			whole1 = Integer.parseInt(f1.substring(0, f1.indexOf("_")));
+			numerator1 = Integer.parseInt(f1.substring(f1.indexOf("_") + 1, f1.indexOf("/")));
+			denominator1 = Integer.parseInt(f1.substring(f1.indexOf("/") + 1));
+			numerator1 = (whole1 * denominator1) + numerator1;
+		}
+
+		else if (f1.contains("/")) {
+			numerator1 = Integer.parseInt(f1.substring(0, f1.indexOf("/")));
+			denominator1 = Integer.parseInt(f1.substring(f1.indexOf("/") + 1));
 			
+		} 
+		
+		
+		else {
+			whole1 = Integer.parseInt(f1.substring(0));
+			numerator1 = whole1;
+			denominator1 = 1;
+		}
+
+		int whole2 = 0;
+		int numerator2 = 0;
+		int denominator2 = 0;
+		if (f2.contains("_")) {
+			whole2 = Integer.parseInt(f2.substring(0, f2.indexOf("_")));
+			numerator2 = Integer.parseInt(f2.substring(f2.indexOf("_") + 1, f2.indexOf("/")));
+			denominator2 = Integer.parseInt(f2.substring(f2.indexOf("/") + 1));
+			numerator2 = (whole2 * denominator2) + numerator2;
+		} 
+		else if (f2.contains("/")) {
+
+			numerator2 = Integer.parseInt(f2.substring(0, f2.indexOf("/")));
+			denominator2 = Integer.parseInt(f2.substring(f2.indexOf("/") + 1));
+			
+		} 
+
+		 else {
+			whole2 = Integer.parseInt(f2.substring(0));
+			numerator2 = whole2;
+			denominator2 = 1;
+		}
+
+		
+		answer = doMath(numerator1, numerator2, denominator1, denominator2, operator);
+		return answer;
+
+	}
+
+	public static String doMath(int n1, int n2, int d1, int d2, String op) {
+
+		String answer = "";
+
+		if (op.equals("+")) {
+			answer = addFrac(n1, n2, d1, d2);
+		} else if (op.equals("-")) {
+			answer = subFrac(n1, n2, d1, d2);
+		}
+
+		else if (op.equals("*")) {
+			answer = multFrac(n1, n2, d1, d2);
+		} else if (op.equals("/")) {
+			answer = divFrac(n1, n2, d1, d2);
+		}
+		return answer;
+	}
+
+	public static String addFrac(int n1, int n2, int d1, int d2) {
+		String answer = "";
+		int numerator = (n1 * d2) + (n2 * d1);
+		int denominator = (d1 * d2);
+		if (n1 == 0) {
+			answer = n2 + "/" + d2;
+		} else if (n2 == 0) {
+			answer = n1 + "/" + d1;
+		} else {
+			answer = numerator + "/" + denominator;
+		}
+		return answer;
+	}
+
+	public static String subFrac(int n1, int n2, int d1, int d2) {
+		n2 = -1 * n2;
+		String answer = "";
+		int numerator = (n1 * d2) + (n2 * d2);
+		int denominator = (d1 * d2);
+		if (n1 == 0) {
+			answer = n2 + "/" + d2;
+		} else if (n2 == 0) {
+			answer = n1 + "/" + d1;
+		} else {
+			answer = numerator + "/" + denominator;
+		}
+		return answer;
+	}
+
+	public static String multFrac(int n1, int n2, int d1, int d2) {
+		int numerator = n1 * n2;
+		int denominator = d1 * d2;
+		String answer = numerator + "/" + denominator;
+		return answer;
+	}
+
+	public static String divFrac(int n1, int n2, int d1, int d2) {
+		int a = n2;
+		int b = d2;
+		d2 = a;
+		n2 = b;
+		int numerator = n1 * n2;
+		int denominator = d1 * d2;
+		String answer = numerator + "/" + denominator;
+		return answer;
 	}
 
 	// TODO: Fill in the space below with any helper methods that you think you will
 	// need
 
 }
- 
--- src/test/java/fracCalc/FracCalcTestCheckpoint3.java
+++ src/test/java/fracCalc/FracCalcTestCheckpoint3.java
@@ -9,7 +9,7 @@ public class FracCalcTestCheckpoint3
     @Test public void testCheckpoint3_AdditionWholeNumbers2() {FracCalcTestALL.assertForEarlyCheckpoints(null, null, "1", FracCalc.produceAnswer("2/3 + 1/3"));}
     @Test public void testCheckpoint3_AdditionWholeNumbers7() {FracCalcTestALL.assertForEarlyCheckpoints(null, null, "1021778", FracCalc.produceAnswer("124543 + 897235"));}
     @Test public void testCheckpoint3_AdditionWithNegatives4() {FracCalcTestALL.assertForEarlyCheckpoints(null, null, "-1_1/4", FracCalc.produceAnswer("-3_3/4 + 2_2/4"));}
-    @Test public void testCheckpoint3_AdditionImproperFractionsAndReductions2() {FracCalcTestALL.assertForEarlyCheckpoints(null, null, "1_1/20", FracCalc.produceAnswer("4/5 + 2/8"));}
+    @Test public void testCheckpoint3_AdditionImproperFractionsAndReductions2() { FracCalcTestALL.assertForEarlyCheckpoints(null, null, "1_1/20", FracCalc.produceAnswer("4/5 + 2/8"));}
     @Test public void testCheckpoint3_AdditionCombined4() { FracCalcTestALL.assertForEarlyCheckpoints(null, null, "62_11/19", FracCalc.produceAnswer("0 + 34_543/19"));}
     @Test public void testCheckpoint3_SubtractionSimple3() {FracCalcTestALL.assertForEarlyCheckpoints(null, null, "0", FracCalc.produceAnswer("4_1/2 - 4_1/2"));}
     @Test public void testCheckpoint3_SubtractionWithNegatives3() {FracCalcTestALL.assertForEarlyCheckpoints(null, null, "-1_1/4", FracCalc.produceAnswer("-3_3/4 - -2_2/4"));}
